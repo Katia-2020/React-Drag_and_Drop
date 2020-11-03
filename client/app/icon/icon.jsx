fa46c15e50edd9edc7729714113fb3fc4ea748a3
Katia-2020 <kateflights@gmail.com>
2020-11-03 12:05:09 +0000

    added props and imported svgs to Icon component

--- client/app/icon/icon.jsx
+++ client/app/icon/icon.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import classnames from 'classnames/bind';
+import * as icons from './svg';
 import styles from './icon.scss';
 
-const Icon = () => {
+const cx = classnames.bind(styles);
+
+const Icon = (props) => {
+  const { icon, theme } = props;
+
+  const glyph = icons[icon];
+
   return (
-    <div className={styles.icon}></div>
-  )
-}
+    <i
+      className={cx('icon', {
+        [`icon--theme-${theme}`]: theme,
+      })}
+      dangerouslySetInnerHTML={{ __html: glyth }}
+    />
+  );
+};
 
 export default Icon;
