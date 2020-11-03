ee6816c8fdbee01bd48f0a29f0590676a9034f4f
Katia-2020 <kateflights@gmail.com>
2020-11-03 10:42:57 +0000

    renames to app from App

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './reset.scss';
-import App from './App';
+import App from './app';
 
 const rootElement = document.getElementById('app');
 
