010d592ce5faa3ed8fe13b1a4511c5aef23abe13
Katia-2020 <kateflights@gmail.com>
2020-11-03 15:52:49 +0000

    added props display for Bar

--- client/app/components/bar/bar.jsx
+++ client/app/components/bar/bar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './bar.scss';
 
 const Bar = (props) => {
-  const { theme, width } = props;
+  const { theme, width, display } = props;
 
   const barWidthStyles = {
     width: `${width}%`,
@@ -11,7 +11,7 @@ const Bar = (props) => {
   return (
     <div className={styles['bar-container']}>
       <div
-        className={styles[`bar bar--${theme}`]}
+        className={styles[`bar bar--${theme} bar--display-${display}`]}
         style={barWidthStyles}
       />
     </div>
