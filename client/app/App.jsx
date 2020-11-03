b496b9c575089088be1c5ae403c29307871b5d1d
Katia-2020 <kateflights@gmail.com>
2020-11-03 13:43:53 +0000

    created a stucture of the App, added an array of elements and passed props to the components

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,8 +1,72 @@
 import React from 'react';
-import './reset.scss';
+import { Row, Column } from './components/grid';
+import Icon from './components/icon';
+import Text from './components/text';
+import Button from './components/button';
+import styles from './reset.scss';
 
-const App = () => (
-  <div />
-);
+const files = [
+  {
+    name: 'website.adobe',
+    type: 'adobe',
+    done: true,
+  },
+  {
+    name: 'appdesign.pdf',
+    type: 'pdf',
+    done: false,
+  },
+  {
+    name: 'icon.jpeg',
+    type: 'jpeg',
+    done: false,
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles['drop-drag-container']}>
+
+        <div>
+          <Text text="upload" />
+        </div>
+
+        <div>
+          {files.map((file) => {
+            const { name, type, done } = file;
+            return (
+              <Row direction="row">
+                <Column shrink>
+                  <Icon icon={type} theme={type} />
+                </Column>
+
+                <Column grow>
+                  <Text text={name} color={done ? 'blue' : 'grey'} />
+                </Column>
+
+                <Column shrink>
+                  <Button icon={done ? 'done' : 'cancel'} theme={type} />
+                </Column>
+              </Row>
+            );
+          })}
+        </div>
+
+        <div>
+        </div>
+
+      </div>
+
+    )
+  }
+}
 
 export default App;
