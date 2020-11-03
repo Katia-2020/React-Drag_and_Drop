cba979f25a5a9b26b3ff959247a5815821e2cf7a
Katia-2020 <kateflights@gmail.com>
2020-11-03 11:30:27 +0000

    created Icon component

---
+++ client/app/icon/icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './icon.scss';
+
+const Icon = () => {
+  return (
+    <div className={styles.icon}></div>
+  )
+}
+
+export default Icon;
