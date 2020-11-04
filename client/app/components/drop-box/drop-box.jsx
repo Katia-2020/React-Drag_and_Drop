2dd844dd8d5034971688247f957f00229facecaa
unknown <dlorenzo.05@gmail.com>
2020-11-04 13:16:06 +0100

    unused onClick event removed, unecesary onDrop event removed, onChanged event added and called and event accessed to get the value of the file uploaded, right type of input added on render

--- client/app/components/drop-box/drop-box.jsx
+++ client/app/components/drop-box/drop-box.jsx
@@ -5,24 +5,14 @@ import { Row, Column } from '../grid';
 import styles from './drop-box.scss';
 
 const DropBox = (props) => {
-  const { onDrop, onClick } = props;
+  const { onChange } = props;
 
-  const handleOnDrop = () => {
-    onDrop();
+  const handleOnChange = (event) => {
+    onChange(event);
   };
-
-  const handleButtonClick = () => {
-    onClick();
-  };
-
-  const fileInput = React.createRef();
-
-  // const handleInputClick = () => {
-  //   onClick();
-  // };
 
   return (
-    <div className={styles['drop-box']} onDragOver="return false" onDrop={handleOnDrop}>
+    <div className={styles['drop-box']}>
       <Row direction="row">
         <Column>
           <Text
@@ -33,21 +23,24 @@ const DropBox = (props) => {
           />
         </Column>
         <Column>
-          {/* <input
-            type="file"
-            className={styles['input']}
-            onClick={handleInputClick}
-          /> */}
-          <input type="file" ref={fileInput} />
-          <Text
-            text="browse"
-            color="blue"
-            size="small"
-            bold="bold"
-            margin="left"
-            cursor="pointer"
-            onClick={handleButtonClick}
-          />
+          <label className={styles['drop-box__label']} htmlFor="choose-files">
+            <input
+              type="file"
+              className={styles['input']}
+              name="choose-files"
+              id="choose-files"
+              onChange={handleOnChange}
+              multiple
+            />
+            <Text
+              text="browse"
+              color="blue"
+              size="small"
+              bold="bold"
+              margin="left"
+              cursor="pointer"
+            />
+          </label>
         </Column>
 
       </Row>
