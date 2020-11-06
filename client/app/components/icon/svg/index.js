fcf7037bfa54dcfeb785658f36cec682fa40eb6b
Katia-2020 <kateflights@gmail.com>
2020-11-06 21:55:32 +0000

    added excel as another valid extension and updated component info in Icon and Spinner

--- client/app/components/icon/svg/index.js
+++ client/app/components/icon/svg/index.js
@@ -1,10 +1,10 @@
-import adobe from './adobe.svg';
+import excel from './excel.svg';
 import jpeg from './jpeg.svg';
 import pdf from './pdf.svg';
 import word from './word.svg';
 
 export {
-  adobe,
+  excel,
   jpeg,
   pdf,
   word,
