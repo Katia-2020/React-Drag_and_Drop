de1271cbfc1259a5f42d711ad13a2dc311b98ca2
Katia-2020 <kateflights@gmail.com>
2020-11-09 20:37:40 +0000

    added gutters to the Row Grid component

--- client/app/components/grid/row/row.jsx
+++ client/app/components/grid/row/row.jsx
@@ -6,13 +6,14 @@ import styles from './row.scss';
 const cx = classnames.bind(styles);
 
 const Row = (props) => {
-  const { direction, children, center } = props;
+  const { direction, children, center, gutters } = props;
 
   return (
     <div
       className={cx('grid', {
         [`grid--${direction}`]: direction,
         'grid--justify-center': center,
+        [`grid--gutters-${gutters}`]: gutters,
       })}
     >
       {children}
