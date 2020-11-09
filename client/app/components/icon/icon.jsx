09b9e4a4a2541bf5be7e688027737f7f86d4da07
Katia-2020 <kateflights@gmail.com>
2020-11-09 20:37:03 +0000

    passes size props to the component Icon

--- client/app/components/icon/icon.jsx
+++ client/app/components/icon/icon.jsx
@@ -6,13 +6,14 @@ import styles from './icon.scss';
 const cx = classnames.bind(styles);
 
 const Icon = (props) => {
-  const { icon, theme } = props;
+  const { icon, theme, size } = props;
   const glyph = icons[icon];
 
   return (
     <i
       className={cx('icon', {
         [`icon--theme-${theme}`]: theme,
+        [`icon--size-${size}`]: size,
       })}
       dangerouslySetInnerHTML={{ __html: glyph }}
     />
